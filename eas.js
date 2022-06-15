f1f169c8f34e05a6656d13af021efbc5ce2c7fc1
getlucky13 <100950736+getlucky13@users.noreply.github.com>
2022-06-15 15:10:16 -0400

    Add notes
    Added comment to bottom of doc with notes on where I'm at currently.

--- eas.js
+++ eas.js
@@ -1,2 +1,9 @@
 const gridCells = document.querySelectorAll('div.grid-row div');
 gridCells.forEach(div => div.addEventListener('mouseover', () => div.style.background = 'black'));
+
+
+/*
+This code currently achieves goal of applying black backround to each cell on mouseoever. However, would like to update in future
+to apply styling via class toggling instead of inline css.
+Next step - need to come up with method of generating grid via JS now that html and css testing is complete. 
+*/
