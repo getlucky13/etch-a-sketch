a548aa5820c4dd9cae155483799c0973f0964c54
Ron Rizzo <churchmouth13@gmail.com>
2022-06-16 21:57:05 -0400

    Format css and html, add js to make grid rows
    Made multiple changes to naming to assure consistency across html and
    css. Added js to create a grid row. Currently in a for loop, but
    isn't executing in the loop.

--- eas.js
+++ eas.js
@@ -1,9 +1,14 @@
-const gridCells = document.querySelectorAll('div.grid-row div');
-gridCells.forEach(div => div.addEventListener('mouseover', () => div.style.background = 'black'));
+function makeGrid() {
+    for(let i = 1; i > 16; i++) {
+        const gridContainer = document.querySelector('.container');
+        const gridRow = document.createElement('div');
+        gridRow.classList.toggle('row');
+        gridContainer.appendChild(gridRow);
+    }
+}
 
+makeGrid();
 
-/*
-This code currently achieves goal of applying black backround to each cell on mouseoever. However, would like to update in future
-to apply styling via class toggling instead of inline css.
-Next step - need to come up with method of generating grid via JS now that html and css testing is complete. 
+/*const gridCells = document.querySelectorAll('div.row div');
+gridCells.forEach(div => div.addEventListener('mouseover', () => div.classList.add('cell-hover')));
 */
